6b94f51bece32d9e585ede17d313afcad5c7a428
abhay-on-it <abhayagnihotri1585@gmail.com>
2024-03-17 02:49:12 +0530

    added navigation bar

--- Sakha/src/Components/Button.jsx
+++ Sakha/src/Components/Button.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
-function Button({ label, bgColor, url }) {
+function Button({ label, bgColor, url,hvrColor ,bdrColor }) {
+  const buttonClassName = useMemo(() => `bg-${bgColor} bg-primary hover:bg-${hvrColor} border border-${bdrColor} sm:px-14 text-nowrap sm:py-5 text-white sm:text-2xl sm:w-1/4 w-1/2 text-xl p-2 flex items-center justify-center font-medium rounded-full`, [bgColor,hvrColor]);
+
   return (
-    <a href={url} style={{backgroundColor:bgColor}} className='sm:px-14 text-nowrap sm:py-6 text-white sm:text-2xl sm:w-1/4 w-1/2 text-xl p-2 flex items-center justify-center font-medium rounded-full'>
+    <a href={url} className={buttonClassName}>
       {label}
     </a>
   );
--- Sakha/src/Sections/Hero.jsx
+++ Sakha/src/Sections/Hero.jsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Button from '../Components/Button';
 import { RxHamburgerMenu } from "react-icons/rx";
-
+import { motion } from 'framer-motion';
+import { IoClose } from "react-icons/io5";
 function Hero() {
   const [phraseIndex, setPhraseIndex] = useState(0);
   const phrases1 = ["20", "300", "15"];
   const phrases2 = ["Years on the Market", "Projects Completed", "Website Awards"];
+  const [isNavigationOpen, setIsNavigationOpen] = useState(false);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -13,31 +15,77 @@ function Hero() {
     }, 2000);
 
     return () => clearInterval(intervalId);
-  }, []);
+  }, [setPhraseIndex]);
 
+  const openNavigation = useCallback(()=>{
+    setIsNavigationOpen(prev => !prev)
+  },[setIsNavigationOpen])
+  
   return (
-    <div className='px-3 sm:px-16'>
+    <div className='relative w-full min-h-screen overflow-hidden px-3 sm:px-16'>
+      <motion.div 
+      initial={{y:"-100%"}}
+      animate={{ y: isNavigationOpen ? "0%" : "-100%" }}
+      transition={{duration:0.5,ease:[0.5, 1, 0.89, 1]}}
+      className='absolute flex items-center justify-center left-0 overlay z-10 w-full h-full bg-[#a0a0a0a4] backdrop-blur-sm'>
+         <div className='sm:w-[60%] sm:h-[85%] flex flex-col rounded-3xl bg-[#000] px-12 py-10'>
+          <div className='relative flex justify-end items-center p-1 w-full sm:h-[4vmax] '>
+            <div 
+            onClick={openNavigation}
+            className='w-[3vmax] h-[3vmax] flex items-center justify-center sm:text-[2vmax] text-white rounded-full bg-[#58555565]'><IoClose /></div>
+          </div>
+           {["Case Studies","Our Agency","Contact Us"].map((item,index)=>{
+            return <h1 className='text-white sm:text-[5vmax]' key={index}>{item}</h1>
+           })}
+         <div className='w-full sm:h-[5vmax] sm:mt-10 flex items-center justify-between '>
+          <div className='w-[60%] flex items-center  justify-start p-2 sm:text-[1.2vmax] text-white gap-6 h-full'>
+            {["Instagram","Facebook","WhatsApp","MobileNo."].map((item,index)=>{
+              return <a href="#" key={index} className={`${index===3 ? "border border-zinc-100 rounded-full px-8 py-1 hover:bg-primary" : "border-b border-transparent hover:border-zinc-100"} `}>{item}</a>
+            })}
+
+          </div>
+              <Button label={"Emergency"} url={"#"} bgColor={"transparent"} hvrColor={"primary"} bdrColor={"bg-zinc-100"} />
+         </div>
+
+         </div>
+      </motion.div>
       <div className='w-full flex justify-between items-center sm:py-6 py-2 px-1'>
         <h1 className='sm:text-4xl text-2xl font-semibold'>LOGO</h1>
         <div className='flex gap-4'>
           <button className='bg-transparent border border-zinc-900 rounded-full px-5 py-1 sm:px-8 sm:py-1'>Get in touch</button>
-          <button className='border border-zinc-900 p-3 sm:text-lg rounded-full'><RxHamburgerMenu /></button>
+          <button 
+          onClick={openNavigation}
+          className='border border-zinc-900 p-3 sm:text-lg rounded-full hover:bg-black hover:text-white'><RxHamburgerMenu /></button>
         </div>
       </div>
       <div className='sm:mt-[6vmax] mt-14 overflow-hidden'>
-        <h1 className='sm:text-[8vmax]  sm:leading-[8vmax] font-medium text-[6vmax] leading-[7vmax]'>Crafting <span className='inline-block text-[#545CFF]'>Digital</span></h1>
-        <h1 className='sm:text-[8vmax]  sm:leading-[8vmax] font-medium text-[6vmax] leading-[7vmax]'>Experiences</h1>
+        <motion.h1
+        initial={{y:"100%"}}
+        animate={{y:"0%"}}
+        transition={{ease:[0.87, 0, 0.13, 1]  }}
+         className='sm:text-[8vmax]  sm:leading-[9vmax] font-medium text-[6vmax] leading-[7vmax]'>Crafting <span className='inline-block text-primary'>Digital</span></motion.h1>
+        <h1 className='sm:text-[8vmax] overflow-hidden sm:leading-[9vmax] font-medium text-[6vmax] leading-[7vmax]'>
+        {"Experiences".split("").map((item,index)=>{
+                            return <motion.span 
+                            initial={{y:"100%"}}
+                            animate={{y:"0%"}}
+                            transition={{ease:[0.22, 1, 0.36, 1],delay:index*0.1}}
+                            key={index}
+                            className='inline-block'
+                            >{item}</motion.span>
+                        })}
+                        </h1>
       </div>
       <div className='sm:mt-24 sm:py-12 w-full gap-12 flex flex-col items-start px-1 sm:flex-row sm:items-center justify-between pt-12'>
-        <div className='sm:w-[25vmax] w-full flex items-center justify-betweens gap-5 h-full'>
-          <div className='sm:w-[4.5vmax] sm:h-[4.5vmax] w-[6vmax] h-[6vmax] p-5 bg-black rounded-full'>
-            <div style={{ transition: '0.3s', ease:'0.76, 0, 0.24, 1' }} className='sm:text-xl text-lg w-full h-full flex items-center justify-center text-center font-medium text-white'>{phrases1[phraseIndex]}</div>
+        <div className='sm:w-[25vmax] w-full flex items-center justify-start sm:gap-10 gap-3 h-full'>
+          <div className='sm:w-[4.5vmax] sm:h-[4.5vmax] w-[7vmax] h-[7vmax] p-5 bg-black rounded-full'>
+            <div style={{ transition: '0.3s', ease:'0.76, 0, 0.24, 1' }} className='sm:text-2xl text-lg w-full h-full flex items-center justify-center text-center font-medium text-white'>{phrases1[phraseIndex]}</div>
           </div>
-          <h1 style={{ transition: '0.3s', ease:'0.76, 0, 0.24, 1' }} className='text-2xl text-nowrap font-medium flex'>{phrases2[phraseIndex]}</h1>
+          <h1 style={{ transition: '0.3s', ease:'0.76, 0, 0.24, 1' }} className='text-2xl text-nowrap font-medium flex '>{phrases2[phraseIndex]}</h1>
         </div>
         <div className='gray  flex flex-col sm:flex sm:flex-row sm:items-center  justify-end gap-10 h-full'>
           <p className='sm:w-1/3 sm:text-2xl text-xl w-[90%]'>We build engaging websites, brands & innovative e-commerce solutions.</p>
-          <Button label="Case Studies" bgColor="#545CFF" url="#" />
+          <Button label="Case Studies" bgColor={"primary"} url="#" hvrColor={"black"} />
         </div>
       </div>
     </div>
