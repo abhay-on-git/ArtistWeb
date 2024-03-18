14ac571bfc45fd2968ce05bf6afecbec204c5573
abhay-on-it <abhayagnihotri1585@gmail.com>
2024-03-18 15:19:07 +0530

    added Locomotive

--- Sakha/src/App.jsx
+++ Sakha/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Hero from './Sections/Hero'
 import Services from './Sections/Services'
-
+import LocomotiveScroll from 'locomotive-scroll'
 const App = () => {
+  const locomotivScroll = new LocomotiveScroll();
   return (
     <div className='w-full min-h-screen bg-zinc-100'>
       <Hero />
